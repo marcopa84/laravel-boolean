bcabef1b47559dfde7b8d0c6cb284a364857b900
Marco <marcopa84@hotmail.it>
2020-03-17 15:52:46 +0100

    layout OK!

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,88 @@
+@include('layouts.partials._header')
+
+
+<header>
+   <div id="navbarContainer">
+        <div class="container-fluid">
+            <nav class="boolean__navbar navbar navbar-expand-lg navbar-light bg-light">
+                <a class="boolean__navbar__logo navbar-brand" href="https://www.boolean.careers">
+                <img src="https://www.boolean.careers/images/common/logo.png" alt="Boolean Careers"></a>
+                <button class="navbar-toggler boolean__navbar__button" type="button">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="boolean__navbar__items collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="boolean__navbar__item nav-item">
+                            <a class="nav-link" href="https://www.boolean.careers">Home</a>
+                        </li>
+                        <li class="boolean__navbar__item nav-item">
+                            <a class="nav-link" href="https://www.boolean.careers/corso/full-time">Corso</a>
+                        </li>
+                        
+                        <li class="boolean__navbar__item nav-item active">
+                            <a class="nav-link" href="https://www.boolean.careers/carriere">Dopo il corso</a>
+                        </li>
+                        
+                        <li class="boolean__navbar__item nav-item">
+                            <a class="nav-link" href="https://www.boolean.careers/lezione-gratuita">Lezione Gratuita</a>
+                        </li>
+                        <li class="boolean__navbar__cta nav-item">
+                            <a track="Click-IscrizioneForm" class="nav-link" href="https://www.boolean.careers/iscriviti">Candidati ora</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<main>
+    @yield('content')
+</main>
+
+<footer class="bg_blue">
+    <div class="container">
+        <div class="footer__head">
+            <div class="footer__head__left">
+                <img src="https://www.boolean.careers/images/common/logo-white.png" alt="Logo White">
+                <div class="footer__head__left__numbers">
+                  Sede Operativa<br>
+                  Via Carducci 12 - 20123 Milano<br>
+                  Tel: 02-36599712<br>
+                </div>
+            </div>
+            <div class="footer__head__right">
+                <ul>
+                    <li><a href="https://www.boolean.careers/lezione-gratuita">Lezione gratuita</a></li>
+                    
+                    
+                    <li><a href="https://www.boolean.careers/faq">Domande frequenti</a></li>
+                    <li><a href="https://www.boolean.careers/aziende">Per le aziende</a></li>
+                    <li><a href="https://www.boolean.careers/privacy-policy">Privacy policy</a></li>
+                    <li><a href="https://www.boolean.careers/work-with-us">Lavora con noi</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <div class="footer__bottom__icons">
+                
+                <a rel="nofollow" href="https://instagram.com/boolean.careers"><i class="fab fa-instagram"></i></a>
+                <a rel="nofollow" href="https://www.facebook.com/boolean.careers"><i class="fab fa-facebook-f"></i></a>
+                <a rel="nofollow" href="https://www.linkedin.com/school/boolean-careers/"><i class="fab fa-linkedin-in"></i></a>
+                <a rel="nofollow" href="https://www.youtube.com/channel/UCN0mktfmWySUg3OmvHKrdcQ"><i class="fab fa-youtube"></i></a>
+            </div>
+            <div class="footer__bottom__text">
+                Boolean SRL - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967
+            </div>
+        </div>
+    </div>
+
+    
+</footer>
+
+
+
+    
+
+@include('layouts.partials._footer')
---
+++ resources/views/layouts/partials/_header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://www.boolean.careers/css/app.css?id=59f5776daca3db25adc3">
+    <title>Document</title>
+</head>
+
+<body>
